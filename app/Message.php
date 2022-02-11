058f9d01842bfd6fbb999e250880643fef15d322
Bravehearts19 <leo18111998@gmail.com>
2022-02-11 17:32:56 +0100

    Message model relation updated

--- app/Message.php
+++ app/Message.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     protected $fillable = ["message","name","email","apartment_id"];
+
+    public function apartment() {
+        return $this->belongsTo("App\Apartment");
+    }
 }
