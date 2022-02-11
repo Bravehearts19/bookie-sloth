6450cb01b8a2cabb12e203526efc503f579801d2
Obez99 <giuliogx@live.it>
2022-02-11 11:07:51 +0100

    Added Service and Sponsor mrcs

---
+++ database/migrations/2022_02_11_092955_create_sponsors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSponsorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->enum('level', ['no_sponsor', 'bronze', 'silver', 'gold'])->unique()->default('no_sponsor');
+            $table->float('price', 3, 2);
+            $table->number('duration');
+            $table->string('badge');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsors');
+    }
+}
