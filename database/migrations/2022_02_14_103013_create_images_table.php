fb7b7ba5be0c1b84c765b7d0faeb26eada92a819
Bravehearts19 <leo18111998@gmail.com>
2022-02-14 11:53:02 +0100

    Created images table with FK

---
+++ database/migrations/2022_02_14_103013_create_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string("img_url");
+            $table->foreignId("apartment_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropForeign('images_apartment_id_foreign');    /* per togliere la foreign key */
+            $table->dropColumn('apartment_id');   /* per cancellare la colonna */
+        });
+
+        Schema::dropIfExists('images');
+    }
+}
