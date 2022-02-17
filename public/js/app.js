a6bb28f260d9a92488f180839329e9a8394659a0
Obez99 <giuliogx@live.it>
2022-02-17 19:46:06 +0100

    Public files update

--- public/js/app.js
+++ public/js/app.js
@@ -27678,9 +27678,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\npala\boolean\bookie-sloth\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\npala\boolean\bookie-sloth\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\npala\boolean\bookie-sloth\resources\sass\_custom.scss */"./resources/sass/_custom.scss");
+__webpack_require__(/*! C:\Users\Work\Desktop\Boolean\Progetto Finale\bookie-sloth\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Work\Desktop\Boolean\Progetto Finale\bookie-sloth\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Work\Desktop\Boolean\Progetto Finale\bookie-sloth\resources\sass\_custom.scss */"./resources/sass/_custom.scss");
 
 
 /***/ })
