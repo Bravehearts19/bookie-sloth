3992b70ce7157140c0ff2ea08c4f03bc1755ca64
Bravehearts19 <leo18111998@gmail.com>
2022-02-18 13:43:35 +0100

    Map in Apartment.vue added

--- resources/js/pages/Apartment.vue
+++ resources/js/pages/Apartment.vue
@@ -1,8 +1,12 @@
 <template>
-    <div>
+    <div class="container">
         <h1 class="text-primary text-center">
             {{ apartment.name }}
         </h1>
+
+        <div class="d-flex justify-content-center">
+            <div id="map-div"></div>
+        </div>
     </div>
 </template>
 
@@ -19,11 +23,28 @@ export default {
         .then((resp) => {
             console.log(resp.data);
             this.apartment = resp.data[this.$route.params.id - 1]; /* la resp.data ritorna un oggetto con chiave id dell'oggetto -1 */
+
+            const HOTEL_COORDINATES = {lng: this.apartment.x_coordinate, lat: this.apartment.y_coordinate};
+
+            const API_KEY = 'onx0t6tyRKJCe8Q2JIAWTMwu3Opxi7wH';
+            const APPLICATION_NAME = 'bookie_sloth';
+            const APPLICATION_VERSION = '1.0';
+
+            tt.setProductInfo(APPLICATION_NAME, APPLICATION_VERSION);
+            var map = tt.map({
+            key: API_KEY,
+            container: 'map-div',
+            center: HOTEL_COORDINATES,
+            zoom: 18
+            });
         })
     }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+#map-div{
+    height: 400px;
+    width: 400px;
+}
 </style>
