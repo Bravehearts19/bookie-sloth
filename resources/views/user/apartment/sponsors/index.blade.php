f0dc2b5060a31cf418645e41664f329e5048d802
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2022-02-19 15:41:26 +0100

    create form for sponsor options

---
+++ resources/views/user/apartment/sponsors/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="mb-3">
+  <label class="form-label">Level</label>
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Scegli il livello del tuo sponsor</option>
+  <option value="1">Oro</option>
+  <option value="2">Argento</option>
+  <option value="3">Bronzo</option>
+</select>
+</div>
+
+@endsection
+
