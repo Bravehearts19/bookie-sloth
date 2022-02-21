a8d460843f66a596e1c5332b6de46c17623af6d7
Obez99 <giuliogx@live.it>
2022-02-21 18:00:18 +0100

    Created Message Controller

--- app/Http/Controllers/User/Apartment/MessageController.php
+++ app/Http/Controllers/User/Apartment/MessageController.php
@@ -4,14 +4,72 @@ namespace App\Http\Controllers\User\Apartment;
 
 use App\Apartment;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use App\Message;
+use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    public function index(Apartment $apartment) {
-        $apartmentId = $apartment->id;
-        $messages = DB::table('messages')->where('apartment_id', $apartmentId)->get();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Apartment $apartment)
+    {
+        $messages = Message::where('apartment_id', $apartment->id)->with('apartment')->get();
 
-        return view('user.apartment.messages.index', ['messages' => $messages, 'apartment' => $apartment]);
+        return view('user.apartment.messages.index', compact("messages"));
+    }
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($apartmentId, $messageId)
+    {
+        $message = Message::where('id', $messageId)
+            ->where('apartment_id', $apartmentId)
+            ->with('apartment')
+            ->first();
+        return view('user.apartment.messages.show', compact('message'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Apartment $apartment, Message $message)
+    {
+        $messageToDelete = Message::where('id', $message->id)
+            ->where('apartment_id', $apartment->id)
+            ->first();
+        Message::softDelete($messageToDelete->id);
+
+        return redirect()->route('user.message.index', $apartment->id);
     }
 }
