961bb6fb99f4e364840c487b413fd697e4521e48
Bravehearts19 <leo18111998@gmail.com>
2022-02-22 13:13:58 +0100

    First tries with Home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,46 +1,46 @@
 <template>
-  <div class="container h-100">
-    <div class="row h-100 align-items-center">
-        <section id="apartments-slider-section" class="col overflow-auto bg-info mt-5">
-            <div class="card-container">
-                <div class="apartment-card py-5 border-top border-bottom border-primary d-flex flex-column justify-content-center"
-                    :key="'hotel-'+index"
-                    v-for="(hotel, index) in hotelArray">
-                    
-                    <div class="apartment-card-header bg-secondary py-3 rounded shadow-lg">
-                        <h1 class="text-white text-center mb-0">{{hotel.name}}</h1>
-                        <h4 class="text-white text-center mb-0">{{hotel.address}} - {{hotel.location}} - {{hotel.cap}}</h4>
+    <div class="h-100 bg-info">
+        <div class="row h-100 align-items-center bg-info">
+            <div class="col">
+                <div class="card-container">
+                    <div class="apartment-card py-5 border-top border-bottom border-primary d-flex flex-column justify-content-center"
+                        :key="'hotel-'+index"
+                        v-for="(hotel, index) in hotelArray">
+                        
+                        <div class="apartment-card-header bg-secondary py-3 rounded shadow-lg">
+                            <h1 class="text-white text-center mb-0">{{hotel.name}}</h1>
+                            <h4 class="text-white text-center mb-0">{{hotel.address}} - {{hotel.location}} - {{hotel.cap}}</h4>
+                        </div>
+
+                        <div class="image-container p-3 mt-5 bg-primary border border-secondary my-3 mx-auto w-50 rounded shadow-lg">
+                            <img :src="hotel.cover_img" :alt="hotel.name" class="w-100 py-3">
+                        </div>
+                        <button class="btn btn-secondary mt-5 w-25 mx-auto">Discover</button>
+                        <div class="d-flex justify-content-around flex-wrap mt-5 py-3 border-top border-secondary bg-primary rounded shadow-lg">
+                            <h6 class="text-secondary mb-0">price: <span class="text-primary">{{hotel.price}}</span></h6>
+                            <h6 class="text-secondary mb-0">bathrooms: {{hotel.n_bathrooms}}</h6>
+                            <h6 class="text-secondary mb-0">guests: {{hotel.n_guests}}</h6>
+                            <h6 class="text-secondary mb-0">rooms: {{hotel.n_rooms}}</h6>
+                            <h6 class="text-secondary mb-0">sizes: {{hotel.size}} mq</h6>
+                        </div>
+                        
                     </div>
 
-                    <div class="image-container p-3 mt-5 bg-primary border border-secondary my-3 mx-auto w-50 rounded shadow-lg">
-                        <img :src="hotel.cover_img" :alt="hotel.name" class="w-100 py-3">
-                    </div>
-                    <button class="btn btn-secondary mt-5 w-25 mx-auto">Discover</button>
-                    <div class="d-flex justify-content-around flex-wrap mt-5 py-3 border-top border-secondary bg-primary rounded shadow-lg">
-                        <h6 class="text-secondary mb-0">price: <span class="text-primary">{{hotel.price}}</span></h6>
-                        <h6 class="text-secondary mb-0">bathrooms: {{hotel.n_bathrooms}}</h6>
-                        <h6 class="text-secondary mb-0">guests: {{hotel.n_guests}}</h6>
-                        <h6 class="text-secondary mb-0">rooms: {{hotel.n_rooms}}</h6>
-                        <h6 class="text-secondary mb-0">sizes: {{hotel.size}} mq</h6>
-                    </div>
-                    
+                    <ul class="pagination overflow-auto">
+                        <li class="page-item"
+                            :class="(index === activePage) ? 'active' : ''"
+                            v-for="index in totalPages"
+                            :key="'page-'+index">
+                            <a href="#" class="page-link"
+                            @click="getHotelData(index)">
+                                {{index}}
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-
-                <ul class="pagination overflow-auto">
-                    <li class="page-item"
-                        :class="(index === activePage) ? 'active' : ''"
-                        v-for="index in totalPages"
-                        :key="'page-'+index">
-                        <a href="#" class="page-link"
-                           @click="getHotelData(index)">
-                            {{index}}
-                        </a>
-                    </li>
-                </ul>
             </div>
-        </section>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -165,5 +165,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../sass/_variables.scss';
+
 
 </style>
