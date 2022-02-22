ebd57252691232f601e0a5ed4d087a861757f9e7
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2022-02-22 18:11:14 +0100

    add backend route for braintree

--- routes/api.php
+++ routes/api.php
@@ -192,6 +192,61 @@ Route::get('/hotelImage', function(Request $request){
     return $imgUrl;
 });
 
+Route::get('/sponsors', function() {
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+    $baseUrl = stripslashes(dirname($_SERVER['SCRIPT_NAME']));
+    $baseUrl = $baseUrl == '/' ? $baseUrl : $baseUrl . '/';
+
+    $token = $gateway->ClientToken()->generate();
+    return view('checkout', [
+        'token' => $token
+    ]);
+});
+
+Route::post('/checkout', function(Request $request) {
+    $gateway = new Braintree\Gateway([
+        'environment' => config('services.braintree.environment'),
+        'merchantId' => config('services.braintree.merchantId'),
+        'publicKey' => config('services.braintree.publicKey'),
+        'privateKey' => config('services.braintree.privateKey')
+    ]);
+    $baseUrl = stripslashes(dirname($_SERVER['SCRIPT_NAME']));
+    $baseUrl = $baseUrl == '/' ? $baseUrl : $baseUrl . '/';
+
+
+    $amount = $request->amount;
+    $nonce = $request->payment_method_nonce;
+
+    $result = $gateway->transaction()->sale([
+        'amount' => $amount,
+        'paymentMethodNonce' => $nonce,
+        'options' => [
+            'submitForSettlement' => true
+        ]
+    ]);
+
+    if ($result->success) {
+        $transaction = $result->transaction;
+        // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+        return back()->with('success_message', 'Transaction successful. The ID is:'. $transaction->id);
+    } else {
+        $errorString = "";
+
+        foreach($result->errors->deepAll() as $error) {
+            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+        }
+
+        // $_SESSION["errors"] = $errorString;
+        // header("Location: " . $baseUrl . "index.php");
+        return back()->withErrors('An error occurred with the message:'. $result->message);
+        
+    }
+});
 
 
 
