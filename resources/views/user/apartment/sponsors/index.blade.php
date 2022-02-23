aa8074cca1449595db47d5b467c1622befd2f181
Obez99 <giuliogx@live.it>
2022-02-23 21:46:56 +0100

    Adds layout and braintree payment box

--- resources/views/user/apartment/sponsors/index.blade.php
+++ resources/views/user/apartment/sponsors/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.user')
 
 @section('content')
 <div class="container-fluid h-100">
@@ -89,6 +89,35 @@
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="container">
+        <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        <div id="dropin-container"></div>
+        <button id="submit-button">Request payment method</button>
+        </div>
+        </div>
+        </div>
+        <script>
+        var button = document.querySelector('#submit-button');
+        braintree.dropin.create({
+        authorization: "sandbox_x6gbkvj7_dw85tnr9p5v7xt7n",
+        container: '#dropin-container'
+        }, function (createErr, instance) {
+        button.addEventListener('click', function () {
+        instance.requestPaymentMethod(function (err, payload) {
+        $.get('{{ route('payment.make') }}', {payload}, function (response) {
+        if (response.success) {
+        alert('Payment successfull!');
+        } else {
+        alert('Payment failed');
+        }
+        }, 'json');
+        });
+        });
+        });
+        </script>
+
+    </body>
 </div>
 @endsection
