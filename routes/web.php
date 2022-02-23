32b9c8750853c9b8d6dcbb160981b718ee182065
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2022-02-23 15:57:15 +0100

    add backend routes for braintree

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Apartment;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,12 +37,66 @@ Route::namespace("User")
         Route::get('/apartment/statistics', 'Apartment\ViewController@index');
 
         Route::get('/apartment/sponsor', 'Apartment\SponsorController@index');
+
+        Route::get('apartment/{apartment}/sponsors', function(Apartment $apartment) {
+            $gateway = new Braintree\Gateway([
+                'environment' => config('services.braintree.environment'),
+                'merchantId' => config('services.braintree.merchantId'),
+                'publicKey' => config('services.braintree.publicKey'),
+                'privateKey' => config('services.braintree.privateKey')
+            ]);
+            $baseUrl = stripslashes(dirname($_SERVER['SCRIPT_NAME']));
+            $baseUrl = $baseUrl == '/' ? $baseUrl : $baseUrl . '/';
+        
+            $token = $gateway->ClientToken()->generate();
+            return view('user\apartment\sponsors\checkout', [
+                'token' => $token, 
+                'apartment_id' => $apartment->id
+            ]);
+        })->name('apartment.{apartment}.sponsors');
+        
+        Route::post('apartment/{apartment}/sponsors', function(Request $request) {
+            $gateway = new Braintree\Gateway([
+                'environment' => config('services.braintree.environment'),
+                'merchantId' => config('services.braintree.merchantId'),
+                'publicKey' => config('services.braintree.publicKey'),
+                'privateKey' => config('services.braintree.privateKey')
+            ]);
+            $baseUrl = stripslashes(dirname($_SERVER['SCRIPT_NAME']));
+            $baseUrl = $baseUrl == '/' ? $baseUrl : $baseUrl . '/';
+        
+        
+            $amount = $request->amount;
+            $nonce = $request->payment_method_nonce;
+        
+            $result = $gateway->transaction()->sale([
+                'amount' => $amount,
+                'paymentMethodNonce' => $nonce,
+                'options' => [
+                    'submitForSettlement' => true
+                ]
+            ]);
+        
+            if ($result->success) {
+                $transaction = $result->transaction;
+                // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+                return back()->with('success_message', 'Transaction successful. The ID is:'. $transaction->id);
+            } else {
+                $errorString = "";
+        
+                foreach($result->errors->deepAll() as $error) {
+                    $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+                }
+        
+                // $_SESSION["errors"] = $errorString;
+                // header("Location: " . $baseUrl . "index.php");
+                return back()->withErrors('An error occurred with the message:'. $result->message);
+                
+            }
+        })->name('apartment.{apartment}.checkout');
+
+        
     });
-
-Route::get('/checkout', function() {
-    return view('user\apartment\sponsors\checkout');
-});
-
 Route::get('{any?}', function () {
     return view('welcome');
 })->where('any', '.*');
