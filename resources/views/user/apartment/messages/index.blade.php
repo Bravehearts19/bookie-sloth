8ac6f3e61f4b76238eaaac74f93005bce640e55b
Obez99 <giuliogx@live.it>
2022-02-25 14:07:37 +0100

    Added date in messages

--- resources/views/user/apartment/messages/index.blade.php
+++ resources/views/user/apartment/messages/index.blade.php
@@ -16,12 +16,18 @@
         <ul class="list-group">
           @foreach($messages as $message)
           <a href="{{route('user.message.show', [$message->apartment->id, $message->id])}}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" aria-current="true">
-            <div>
-              Messaggio da <strong>{{$message->name}}</strong>
+            <div class="d-flex justify-content-between">
+              <p>Messaggio da <strong>{{$message->name}}</strong></p>
             </div>
           
             @if($showAll)
+            <div class="d-flex">
+              <div class="me-3 opacity-50">{{$message->created_at}}</div>
               <h5>{{$message->apartment->name}}</h5>
+            </div>
+
+            @else
+            <span class="ms-3 opacity-50">{{$message->created_at}}</span>
             @endif
           </a>
           @endforeach
