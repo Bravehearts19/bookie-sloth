bf49d6316fec62b0e941baad23b3ea54b8939bc3
LorenzoRottigni <lorenzo@rottigni.net>
2022-02-27 16:45:46 +0100

    advanced search

--- resources/js/components/Services.vue
+++ resources/js/components/Services.vue
@@ -1,34 +1,39 @@
 <template>
   <div>
     <form action="">
-    <h3 class="ms-4"><strong>Servizi :</strong></h3>
-      <ul class="list-unstyled d-flex flex-wrap justify-content-around my-row m-auto bg-secondary">
+    <h2 class="ms-4 text-white text-center mt-3"><strong>Servizi :</strong></h2>
+      <ul class="list-unstyled d-flex flex-wrap justify-content-around my-row m-auto bg-secondary mt-5">
         <li
           v-for="(service, index) in services"
           :key="'service-' + index"
-          class="d-flex align-items-center single-service"
+          class="d-flex align-items-center justify-content-around single-service border-left border-right border-bottom border-primary shadow-lg rounded mt-3"
         >
-          <input
-            class="form-check-input"
-            type="checkbox"
-            @click="selectService(service.id)"
-            id="flexCheckDefault"
-          />
-          <h3 class="mb-0 ps-2 service_title">
-            {{ capitalizeFirstLetter(service.name) }}
-          </h3>
-          <lord-icon
-            :src="service.icon"
-            trigger="loop-on-hover"
-            style="width: 50px; height: 50px"
-          >
-          </lord-icon>
+            <input
+                class="form-check-input m-0 bg-primary"
+                type="checkbox"
+                @click="selectService(service.id)"
+                id="flexCheckDefault"
+            />
+            <div class="d-flex align-items-center">
+              <lord-icon
+                  :src="service.icon"
+                  trigger="loop-on-hover"
+                  style="width: 50px; height: 50px"
+                  colors="primary:#c7ef00,secondary:#ffffff"
+              >
+              </lord-icon>
+              <h3 class="mb-0 text-primary ps-2 service_title">
+                  {{ capitalizeFirstLetter(service.name) }}
+              </h3>
+            </div>
         </li>
       </ul>
     </form>
   </div>
 </template>
 <script>
+import Checkbox from 'primevue/checkbox';
+
 export default {
   name: "Services",
   data() {
@@ -61,7 +66,7 @@ export default {
 <style lang='scss' scoped>
   .my-row{
     width: 100%;
-    
+
 
     .single-service{
       width: calc((100% / 5) - 40px);
