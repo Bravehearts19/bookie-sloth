d04bdbd72c9564d3630a3e1b96876fd77b04cab8
Obez99 <giuliogx@live.it>
2022-02-28 12:44:27 +0100

    Redirects user if sponsor is applied, updates apartment sponsor

--- app/Http/Controllers/User/Apartment/SponsorController.php
+++ app/Http/Controllers/User/Apartment/SponsorController.php
@@ -13,26 +13,31 @@ class SponsorController extends Controller
 {
     public function index(Apartment $apartment)
     {
-        $sponsors = Sponsor::all();
+        if ($apartment->sponsors->first()->level != 'no_sponsor') {
+            return redirect()->route('user.apartment.index');
+        } else {
+            $sponsors = Sponsor::all();
 
-        return view('user.apartment.sponsors.index', ['sponsors' => $sponsors, 'apartment' => $apartment]);
+            return view('user.apartment.sponsors.index', ['sponsors' => $sponsors, 'apartment' => $apartment]);
+        }
     }
 
     public function store(Request $request, Apartment $apartment)
     {
         $data = $request->all();
         $sponsorToApply = Sponsor::where('id', $data["sponsor"])->first();
-        $allSponsors = DB::table('apartment_sponsor')->where('apartment_id', $apartment->id)->get()->toArray();
+        $apartmentSponsor = DB::table('apartment_sponsor')->where('apartment_id', $apartment->id)->get()->toArray();
 
         $expireDate = "";
         if (empty($allSponsors)) {
             $expireDate = Carbon::now()->addHours($sponsorToApply["duration"] + 1)->toDateTimeString();
         } else {
-            $expireDate = Carbon::parse(end($allSponsors)->expires_at)->addHours($sponsorToApply["duration"])->toDateTimeString();
+            $expireDate = Carbon::parse(end($apartmentSponsor)->expires_at)->addHours($sponsorToApply["duration"])->toDateTimeString();
         }
 
-
-        $apartment->sponsors()->attach($sponsorToApply->id, ['expires_at' => $expireDate, 'created_at' => Carbon::now()->addHours(1)]);
+        DB::table('apartment_sponsor')
+            ->where('apartment_id', $apartment->id)
+            ->update(['sponsor_id' => $sponsorToApply['id'], 'expires_at' => $expireDate, 'created_at' => Carbon::now()->addHours(1)]);
 
         return redirect()->route('user.apartment.index')->with('msg', 'Sponsor applicato!');
     }
